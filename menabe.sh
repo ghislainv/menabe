93d7275ebcd411f130fd76a3b7d26607c0b2c0fa
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2016-07-08 17:52:33 +0200

    Montage biodiversity photos

--- menabe.sh
+++ menabe.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# # sizes
+# $h_small=400
+# $w_small=600
+# $h_big=800
+# $w_big=1200
+
 # arachide
 dir="figs/photos/arachide/selected"
 f1="537_Menabe.jpg"
@@ -31,3 +37,10 @@ convert -gravity NorthWest -annotate +610+10 "b)" -pointsize 48 "$dir/a1.jpg" "$
 convert -gravity NorthWest -annotate +10+410 "c)" -pointsize 48 "$dir/a2.jpg" "$dir/a3.jpg"
 mv "$dir/a3.jpg" "figs/mais.jpg"
 rm "$dir/f1.jpg" "$dir/f2.jpg" "$dir/f3.jpg" "$dir/m1.jpg" "$dir/m2.jpg" "$dir/a1.jpg" "$dir/a2.jpg"
+
+# biodiversity
+dir="figs/photos/biodiversity/selected"
+mkdir "$dir/dir_mogr"
+mogrify -path "$dir/dir_mogr" -resize 450x300^ -gravity center -extent 450x300 "$dir/*.jpg"
+montage "$dir/dir_mogr/*.jpg" -tile 4x4 -geometry +0+0 "figs/biodiversity.jpg"
+
